f55616b84efc4b60bf648bc2ab1948ac83b1f221
~ <demichaelson@gmail.com>
2020-11-18 17:33:10 -0600

    Organizing terminals; more notes on the interface

--- interface/notes.md
+++ interface/notes.md
@@ -1,4 +1,56 @@
 
+# General notes
+
+- We want to use attribute accesses as values and as relations on
+  which we can do case analysis.  We need to have some way to talk
+  about attributes having values as well (versus being undefined).
+- We need to be able to generate variables for the tree structure and
+  node when we do a binding.  This relies on typing and knowing the
+  defined relations.
+  + This might rely on some type inference, since we want to be able
+    to write `forall t, ... t.a ...` and be able to figure out what
+    type `t` has.
+- Equality between tree types needs to be equality between the
+  structures, not the nodes.
+- We need to be able to do induction on a tree (actually the WPD
+  relation for it), which requires `induction on L` where `L` is
+  either   numbers or tree names, with the tree names then translated
+  to   numbers in the translated theorems.  We will also need to keep
+  track   of this in bringing the results back from Abella,
+  translating the   restriction on WPD into a restriction on the tree
+  and such.
+- We need some way to say `t.a` exists, preferrably without actually
+  saying `t.a = X` for some value `X`.  This may fall out as I work out
+  the translation, since uses of t.a are going to translate to replace
+  the t.a with something like `t__DOT__a` and add a premise of
+  `access__a__NT t_Node (attr_ex t__DOT__a)`.
+- We need to keep track of the extra things we add to theorems, or
+  some standard way of figuring out what we add.  When the user
+  applies something with the `apply` tactic, we migth need to add some
+  things to the premises they are applying with.  For example, if we
+  added a WPD premise, we need to give it a WPD premise.  The things
+  we add can probably just be underscores.
+- I eventually want a command to generate the required proof theorems
+  for modular semantic preservation rather than requiring the user to
+  write them.  They are based on case analysis, so they can be written
+  by the machine.
+  * With this, we'll need to do the instantiation of the fake
+    inductive hypotheses ourselves and present them to the user.
+    Abella won't take care of it, since it isn't real Abella
+    induction.
+- We are likely to get multiple Abella commands out of one interface
+  command at some points.  For undoing, we're going to need to keep
+  track of how many commands, so we can undo the correct number of
+  times in Abella.
+- If two accesses of the same attribute come up as separate hypotheses
+  in a proof, we should automatically apply a theorem to show that the
+  values they access are equal.  This is part of treating attribute
+  accesses as values.
+
+
+
+
+
 # Things to think about doing:
 
 - Perhaps keep track of abbreviated hypotheses.  This might be useful
@@ -16,6 +68,30 @@
   hypotheses, to allow analysis on them.
 - Check that all variables being used in a proof are ones that the
   sure actually sees, not hidden ones.
+- We want to hide the fact that we're using relations for the
+  attribute accesses, and so we also want to hide the fact that we're
+  using component relations to enforce the unknowability of children.
+  Therefore we should try to insert applications of theorems allowing
+  us to move logically between the different levels of definitions
+  without the user needing to be aware of them.
+- I might cut down on the allowed identifiers in Abella.  If, for
+  example, we cut `_` from the allowed starting characters for
+  identifiers in the interface, we can start all the encoded relations
+  and identifier names with underscores and be certain the names
+  aren't anything the user is going to overwrite.
+- I really like Coq's semantic bullet points.  It annoys me that
+  Abella doesn't have them.  I would like to add them here.
+- If we end up with the expected encoding of strings (list of
+  characters, where the characters are members of a defined type), we
+  should catch that and replace it with a more normal string-looking
+  string of characters.
+- Abella doesn't have the standard bracketed list syntax as far as I
+  know (e.g. you can't write `[3, 4]`; you have to write `3::4::nil`).
+  This might be nice to include in the interface.
+- Maybe add our own option for `Set`, which could turn on and off
+  displaying the raw Abella proof state.  This might be helpful for
+  debugging, especially if it showed both proof states.  It could be
+  something like `Set interface_debug (true | false).`
 
 
 
@@ -23,11 +99,7 @@
 
 # Abella Questions
 
-- What is the difference between `clear H1 H2.` and `clear -> H1 H2'?
+- What is the difference between `clear H1 H2.` and `clear -> H1 H2`?
   I don't see any difference in result.
 - What is `async.`?
-
-
-
-
 
--- interface/toAbella/concreteSyntax/Terminals.sv
+++ interface/toAbella/concreteSyntax/Terminals.sv
@@ -26,83 +26,78 @@ terminal Theorem_t        'Theorem'        lexer classes {COMMAND};
 terminal TType_t          'Type'           lexer classes {COMMAND};
 
 
-terminal Abbrev_t      'abbrev'       lexer classes {LATER};
-terminal Abort_t       'abort'  lexer classes {TACTIC};
-terminal All_t  'all'       lexer classes {LATER};
-terminal Apply_t  'apply' lexer classes {TACTIC};
-terminal As_t  'as'       lexer classes {LATER};
-terminal Assert_t  'assert' lexer classes {TACTIC};
-terminal Async_t  'async'       lexer classes {LATER};
-terminal Backchain_t  'backchain'  lexer classes {TACTIC};
-terminal By_t  'by'       lexer classes {LATER};
-terminal Case_t  'case'  lexer classes {TACTIC};
-terminal Clear_t  'clear'       lexer classes {LATER};
+terminal Abbrev_t       'abbrev'       lexer classes {COMMAND};
+terminal Abort_t        'abort'        lexer classes {TACTIC};
+terminal All_t          'all'          lexer classes {COMMAND};
+terminal Apply_t        'apply'        lexer classes {TACTIC};
+terminal As_t           'as'           lexer classes {COMMAND};
+terminal Assert_t       'assert'       lexer classes {TACTIC};
+terminal Async_t        'async'        lexer classes {TACTIC};
+terminal Backchain_t    'backchain'    lexer classes {TACTIC};
+terminal By_t           'by'           lexer classes {COMMAND};
+terminal Case_t         'case'         lexer classes {TACTIC};
+terminal Clear_t        'clear'        lexer classes {TACTIC};
 terminal Coinduction_t  'coinduction'  lexer classes {TACTIC};
-terminal End_t  'end'       lexer classes {LATER};
-terminal Exists_t  'exists'  lexer classes {TACTIC, LOGIC};
-terminal False_t  'false'  lexer classes {LOGIC};
-terminal Forall_t  'forall'  lexer classes {LOGIC};
-terminal From_t  'from'       lexer classes {LATER};
-terminal Induction_t  'induction'  lexer classes {TACTIC};
-terminal Intros_t  'intros'  lexer classes {TACTIC};
-terminal Keep_t  'keep'       lexer classes {LATER};
-terminal Left_t  'left'  lexer classes {TACTIC};
-terminal Nabla_t  'nabla'  lexer classes {LOGIC};
-terminal On_t  'on'       lexer classes {LATER};
-terminal Permute_t  'permute'  lexer classes {TACTIC};
-terminal Rename_t  'rename'  lexer classes {TACTIC};
-terminal Right_t  'right'  lexer classes {TACTIC};
-terminal Search_t  'search'  lexer classes {TACTIC};
-terminal Skip_t  'skip'  lexer classes {TACTIC};
-terminal Split_t  'split'  lexer classes {TACTIC};
-terminal SplitStar_t  'split*'  lexer classes {TACTIC};
-terminal To_t  'to'       lexer classes {LATER};
-terminal True_t  'true'  lexer classes {LOGIC};
-terminal Type_t  'type'  lexer classes {LOGIC};
-terminal Unabbrev_t  'unabbrev'       lexer classes {LATER};
-terminal Undo_t  'undo'  lexer classes {TACTIC};
-terminal Unfold_t  'unfold'  lexer classes {TACTIC};
-terminal With_t  'with'       lexer classes {LATER};
-terminal Witness_t  'witness'  lexer classes {TACTIC};
+terminal Exists_t       'exists'       lexer classes {TACTIC, LOGIC};
+terminal False_t        'false'        lexer classes {LOGIC};
+terminal Forall_t       'forall'       lexer classes {LOGIC};
+terminal Induction_t    'induction'    lexer classes {TACTIC};
+terminal Intros_t       'intros'       lexer classes {TACTIC};
+terminal Keep_t         'keep'         lexer classes {TACTIC};
+terminal Left_t         'left'         lexer classes {TACTIC};
+terminal Nabla_t        'nabla'        lexer classes {LOGIC};
+terminal On_t           'on'           lexer classes {TACTIC};
+terminal Permute_t      'permute'      lexer classes {TACTIC};
+terminal Rename_t       'rename'       lexer classes {TACTIC};
+terminal Right_t        'right'        lexer classes {TACTIC};
+terminal Search_t       'search'       lexer classes {TACTIC};
+terminal Skip_t         'skip'         lexer classes {TACTIC};
+terminal Split_t        'split'        lexer classes {TACTIC};
+terminal SplitStar_t    'split*'       lexer classes {TACTIC};
+terminal To_t           'to'           lexer classes {TACTIC};
+terminal True_t         'true'         lexer classes {LOGIC};
+terminal Type_t         'type'         lexer classes {LOGIC};
+terminal Unabbrev_t     'unabbrev'     lexer classes {COMMAND};
+terminal Undo_t         'undo'         lexer classes {TACTIC};
+terminal Unfold_t       'unfold'       lexer classes {TACTIC};
+terminal With_t         'with'         lexer classes {TACTIC};
+terminal Witness_t      'witness'      lexer classes {TACTIC};
 
 
-terminal Back_t        '#back'  lexer classes {TOKEN};
+terminal Back_t        '#back'   lexer classes {TOKEN};
 terminal Reset_t       '#reset'  lexer classes {TOKEN};
-terminal DefEq_t       ':='  lexer classes {TOKEN};
-terminal Comma_t       ','  lexer classes {TOKEN}, precedence=3;
-terminal Period_t      '.'  lexer classes {TOKEN};
-terminal Semicolon_t   ';'  lexer classes {TOKEN};
-terminal Backslash_t   '\'  lexer classes {TOKEN}, precedence=7;
-terminal LParen_t      '('  lexer classes {TOKEN};
-terminal RParen_t      ')'  lexer classes {TOKEN};
-terminal Eq_t          '='  lexer classes {TOKEN};
-terminal Colon_t       ':'  lexer classes {TOKEN};
-terminal RightArrow_t  '->'  lexer classes {TOKEN}, association=right, precedence=4;
-terminal Star_t        '*'  lexer classes {TOKEN};
-terminal At_t          '@'  lexer classes {TOKEN};
-terminal Octothorpe_t  '#'  lexer classes {TOKEN};
-terminal Plus_t        '+'  lexer classes {TOKEN};
-terminal Or_t          '\/'  lexer classes {TOKEN}, precedence=5, association=left;
-terminal And_t         '/\'     lexer classes {TOKEN}, precedence=6, association=left;
-terminal LBracket_t    '['   lexer classes {TOKEN};
-terminal RBracket_t    ']'   lexer classes {TOKEN};
-terminal Underscore_t  '_'  lexer classes {TOKEN};
-
-terminal OptSemi_t  /;?/  lexer classes{TOKEN};
+terminal DefEq_t       ':='      lexer classes {TOKEN};
+terminal Comma_t       ','       lexer classes {TOKEN}, precedence=3;
+terminal Period_t      '.'       lexer classes {TOKEN};
+terminal Semicolon_t   ';'       lexer classes {TOKEN};
+terminal Backslash_t   '\'       lexer classes {TOKEN}, precedence=7;
+terminal LParen_t      '('       lexer classes {TOKEN};
+terminal RParen_t      ')'       lexer classes {TOKEN};
+terminal Eq_t          '='       lexer classes {TOKEN};
+terminal Colon_t       ':'       lexer classes {TOKEN};
+terminal RightArrow_t  '->'      lexer classes {TOKEN}, precedence=4, association=right;
+terminal Star_t        '*'       lexer classes {TOKEN};
+terminal At_t          '@'       lexer classes {TOKEN};
+terminal Octothorpe_t  '#'       lexer classes {TOKEN};
+terminal Plus_t        '+'       lexer classes {TOKEN};
+terminal Or_t          '\/'      lexer classes {TOKEN}, precedence=5, association=left;
+terminal And_t         '/\'      lexer classes {TOKEN}, precedence=6, association=left;
+terminal LBracket_t    '['       lexer classes {TOKEN};
+terminal RBracket_t    ']'       lexer classes {TOKEN};
+terminal Underscore_t  '_'       lexer classes {TOKEN};
+terminal OptSemi_t     /;?/      lexer classes {TOKEN};
 
 
 terminal Id_t  /[-A-Za-z^=`'?$][-A-Za-z^=`'?$0-9_*@+#!~\/]*/;
-
-
 terminal QString_t  /"[^"]*"/;
-
-
 terminal Number_t  /[0-9]+/;
 
 
-ignore terminal Whitespace_t /[\ \t\n\r]+/;
+terminal AttrAccess_t /[a-z][A-Za-z0-9\_]*\.[a-z][A-Za-z0-9\_]*/;
 
--- Allows (one level of) nested comments.  Based on Silver comments
+
+ignore terminal Whitespace_t /[\ \t\n\r]+/;
+-- Allows (one level of) nested comments.  Based on Silver comments.
 ignore terminal BlockComment_t /\/\*(\/\*([^\*]|\*+[^\/\*])*\*+\/|[^\*]|\*+[^\/\*])*\*+\// lexer classes {COMMENT};
 ignore terminal OneLineComment_t /(%.*)/ lexer classes {COMMENT};
 
