5eefe12f6f3ff94d6cc2e1474997e15bd86a268c
~ <demichaelson@gmail.com>
2021-05-13 13:32:33 -0500

    Fixing reading in commands with multi-line comments

--- interface_/composed/Main.sv
+++ interface_/composed/Main.sv
@@ -277,13 +277,73 @@ function cleanState
 function read_full_input
 IOVal<String> ::= ioin::IO
 {
+  return read_full_input_comments(ioin, 0);
+}
+{-
+  Read the command, keeping track of open multi-line comments to
+  ensure reading in a full command, rather than just part of one and
+  part of an open comment
+-}
+function read_full_input_comments
+IOVal<String> ::= ioin::IO openComments::Integer
+{
   local read::IOVal<String> = readLineStdin(ioin);
-  local readRest::IOVal<String> = read_full_input(read.io);
+  local newOpenComments::Integer =
+        count_comments(read.iovalue, openComments);
+  local readRest::IOVal<String> =
+        read_full_input_comments(read.io, newOpenComments);
   local noWhiteSpace::String = stripExternalWhiteSpace(read.iovalue);
   local shouldEnd::Boolean = endsWith(".", noWhiteSpace);
-  return if shouldEnd
-         then ioval(read.io, read.iovalue)
-         else ioval(readRest.io, read.iovalue ++ "\n" ++ readRest.iovalue);
+  return
+     if openComments < 0
+     then read --syntax error
+     else if openComments > 0
+     then ioval(readRest.io, read.iovalue ++ "\n" ++ readRest.iovalue)
+     else if shouldEnd
+     then ioval(read.io, read.iovalue)
+     else ioval(readRest.io, read.iovalue ++ "\n" ++ readRest.iovalue);
+}
+--Return number of open comments after line
+function count_comments
+Integer ::= line::String openComments::Integer
+{
+  local stringStart::Integer = indexOf("\"", line);
+  local lineStart::Integer = indexOf("%", line);
+  local multiStart::Integer = indexOf("/*", line);
+  local multiEnd::Integer = indexOf("*/", line);
+  return
+     if openComments < 0
+     then openComments --syntax error
+     else if openComments > 0
+     then if multiEnd >= 0
+          then count_comments(substring(multiEnd + 2, length(line),
+                                        line), openComments - 1)
+          else openComments
+     --openComments == 0
+     else if lineStart >= 0 &&
+             (stringStart < 0 || lineStart < stringStart) &&
+             (multiStart < 0 || lineStart < multiStart)
+     then 0 --is line comment, so nothing else matters
+     else if stringStart >= 0 &&
+             (multiStart < 0 || stringStart < lineStart)
+     then count_comments(clear_string(substring(stringStart + 1,
+                                                length(line), line)),
+                         openComments)
+     else if multiStart >= 0
+     then count_comments(substring(multiStart + 2, length(line), line),
+                         openComments + 1)
+     else 0; --nothing special in this line
+}
+--Remove a quoted string from the beginning of a line
+function clear_string
+String ::= line::String
+{
+  local quote::Integer = indexOf("\"", line);
+  local slashquote::Integer = indexOf ("\\\"", line); --\"
+  return
+     if quote < slashquote --quote must be found for valid syntax
+     then substring(quote + 1, length(line), line)
+     else clear_string(substring(slashquote + 2, length(line), line));
 }
 
 
