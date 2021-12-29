3df6615a38e9f6308b3587dcce09e6d5b49f17a8
~ <demichaelson@gmail.com>
2021-12-29 13:52:14 -0600

    More security checking for security extension

--- test_grammars/imp/security/Abstract.sv
+++ test_grammars/imp/security/Abstract.sv
@@ -8,6 +8,9 @@ inherited attribute secCtx::[(String, Integer)] occurs on A, B, C;
 synthesized attribute secLevel::Integer occurs on A, B;
 inherited attribute programCounter::Integer occurs on C;
 synthesized attribute isSecure::Boolean occurs on C;
+--
+synthesized attribute gatherSecCtx::[(String, Integer)] occurs on C;
+synthesized attribute wellFormedSecCtx::Boolean occurs on C;
 
 
 aspect production plus
@@ -87,10 +90,27 @@ top::B ::=
 
 
 
+abstract production assignLevel
+top::C ::= level::Integer name::String
+{
+  top.env_out = top.env;
+
+  top.isSecure = true;
+
+  top.gatherSecCtx = [(name, level)];
+  top.wellFormedSecCtx = true;
+
+  forwards to noop();
+}
+
+
 aspect production noop
 top::C ::=
 {
   top.isSecure = true;
+
+  top.gatherSecCtx = [];
+  top.wellFormedSecCtx = true;
 }
 
 
@@ -102,6 +122,11 @@ top::C ::= c1::C c2::C
   c1.programCounter = top.programCounter;
   c2.programCounter = top.programCounter;
   top.isSecure = c1.isSecure && c2.isSecure;
+
+  top.gatherSecCtx = c1.gatherSecCtx ++ c2.gatherSecCtx;
+  top.wellFormedSecCtx =
+      c1.wellFormedSecCtx && c2.wellFormedSecCtx &&
+      consistentCtxs(c1.secCtx, c2.secCtx);
 }
 
 
@@ -110,6 +135,9 @@ top::C ::= name::String val::A
 {
   val.secCtx = top.secCtx;
   top.isSecure = imp:host:lookup(name, top.secCtx) >= val.secLevel;
+
+  top.gatherSecCtx = [];
+  top.wellFormedSecCtx = true;
 }
 
 
@@ -122,6 +150,11 @@ top::C ::= cond::B th::C el::C
   th.programCounter = max(cond.secLevel, top.programCounter);
   el.programCounter = max(cond.secLevel, top.programCounter);
   top.isSecure = th.isSecure && el.isSecure;
+
+  top.gatherSecCtx = th.gatherSecCtx;
+  top.wellFormedSecCtx =
+      th.wellFormedSecCtx && el.wellFormedSecCtx &&
+      consistentCtxs(th.secCtx, el.secCtx);
 }
 
 
@@ -132,6 +165,9 @@ top::C ::= cond::B body::C
   body.secCtx = top.secCtx;
   body.programCounter = max(cond.secLevel, top.programCounter);
   top.isSecure = body.isSecure;
+
+  top.gatherSecCtx = body.secCtx;
+  top.wellFormedSecCtx = body.wellFormedSecCtx;
 }
 
 
@@ -140,5 +176,36 @@ function max
 Integer ::= i1::Integer i2::Integer
 {
   return if i1 > i2 then i1 else i2;
+}
+
+
+--Check all levels are consistent between the two
+function consistentCtxs
+Boolean ::= ctx1::[(String, Integer)] ctx2::[(String, Integer)]
+{
+  return checkOneCtx(ctx1, ctx2) && checkOneCtx(ctx2, ctx1);
+}
+
+--Check all levels from one are consistent in the other
+function checkOneCtx
+Boolean ::= checkCtx::[(String, Integer)] other::[(String, Integer)]
+{
+  return
+     if null(checkCtx)
+     then true
+     else checkValCtx(fst(head(checkCtx)), snd(head(checkCtx)), other) &&
+          checkOneCtx(tail(checkCtx), other);
+}
+
+--Check other has val for name, or no value
+function checkValCtx
+Boolean ::= name::String val::Integer other::[(String, Integer)]
+{
+  return
+     if null(other)
+     then true
+     else if fst(head(other)) == name
+          then snd(head(other)) == val
+          else checkValCtx(name, val, tail(other));
 }
 
