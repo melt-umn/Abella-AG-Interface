cd93caabaf6155afdb49cdec5581d5b775f5b377
~ <demichaelson@gmail.com>
2022-05-21 16:15:58 -0500

    Adding support scripts and reorganizing

--- silverabella-PG/silverabella.el
+++ silverabella-PG/silverabella.el
@@ -29,7 +29,7 @@
  'silverabella "SilverAbella"
  proof-assistant-home-page  "https://github.com/melt-umn/Abella-AG-Interface"
 
- proof-prog-name  "/home/tux/research/Abella-AG-Interface/interface_/silverabella"
+ proof-prog-name  "silverabella"
 
  proof-context-command                proof-no-regexp
  proof-showproof-command              "Show $$current."
